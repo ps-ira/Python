f0662aac896ad3e57840efe8a4c4ae984ed0578c
ps-ira <purnatasahaira2@gmail.com>
2024-10-13 18:36:57 +0600

    I coded a program to swap 3 numbers

---
+++ Oparetors and Data Types/ACP.py
@@ -0,0 +1,13 @@
+x = input("Enter value of x: ")
+y = input("Enter value of y: ")
+z = input("Enter value of z: ")
+
+temp = x
+x = y
+y = z
+z = temp
+
+print("Value of x after swapping:", x)
+print("Value of y after swapping:", y)
+print("Value of z after swapping:", z)
+6
