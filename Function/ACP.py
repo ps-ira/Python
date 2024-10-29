3e23ca56e1e235e9223fb83ab306404272ace682
ps-ira <purnatasahaira2@gmail.com>
2024-10-29 13:07:41 +0600

    Function to get Fibonacci series

---
+++ Function/ACP.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    a, b = 0, 1
+    count = 0
+    while count < n:
+        print(a, end=' ')
+        a, b = b, a + b
+        count += 1
+
+number = int(input("Enter number to get Fibonacci series")) 
+
+fibonacci(number)
