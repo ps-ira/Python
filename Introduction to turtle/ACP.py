c33f998d5ef83c6670c3daa14bfff23b63c30fab
ps-ira <purnatasahaira2@gmail.com>
2024-11-02 19:16:22 +0600

    I painted a rectangle red

---
+++ Introduction to turtle/ACP.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.Screen().bgcolor("Light green")
+
+r=turtle.Turtle()
+
+r.fillcolor('red')
+r.begin_fill()
+
+r.forward(140)
+r.left(90)
+r.forward(100)
+r.left(90)
+r.forward(140)
+r.left(90)
+r.forward(100)
+
+r.end_fill()
+
+turtle.done()
